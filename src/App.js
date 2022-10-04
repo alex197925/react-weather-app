bd37a1a5d5df2e609d51be7ed298ee9703a50298
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-10-04 17:02:04 +0200

    Fetch data API with axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import axios from 'axios';
 
 
 
 function App() {
-//const url = `https://api.openweathermap.org/data/2.5/weather?q=antwerpen&appid=4068248e6fdf0020b855d97f00767e29`;
-  const [temp, setTemp] = useState([]);
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
    //Get data from API with axios
-   useEffect(() => {
-     const axiosTemp = async () => {
-       const res = await axios('https://api.openweathermap.org/data/2.5/weather?q=antwerpen&appid=4068248e6fdf0020b855d97f00767e29');
-       setTemp(res.data)
-       console.log(res.data);
-     }
-     axiosTemp();
-   }, [])
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=4068248e6fdf0020b855d97f00767e29`;
 
 
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
+      </div>
     <div className="container">
       <div className="top">
         <div className="location">
@@ -36,12 +47,15 @@ function App() {
       <div className="bottom">
         <div className="feels">
           <p>16°C</p>
+          <p className="bold">Feels Like</p>
         </div>
         <div className="humidity">
           <p>20%</p>
+          <p className="bold">Humidity</p>
         </div>
         <div className="wind">
           <p>14 km/h</p>
+          <p className="bold">Wind</p>
         </div>
       </div>
     </div>
