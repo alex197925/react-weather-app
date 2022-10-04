d5843ba4d94a3ff5c89f2bb755b9f6ed7fe375bf
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-10-04 14:56:01 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## React Weather App
 
 Build a React JS weather app using the OpenWeatherMap API. We will be using axios to connect with our API. There are probably a million ways to build a weather app and this is on a more basic level, but a great way to learn a few core fundamentals in React. 
+
+
+1. Install axios
+```typescript
+npm i axios
+```
+2. Import axios
+```typescript
+import axios from 'axios';
+```
--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import React, {useState} from "react";
+import axios from 'axios';
 
-import './App.css';
+
 
 function App() {
+//const url = `https://api.openweathermap.org/data/2.5/weather?q=antwerpen&appid=4068248e6fdf0020b855d97f00767e29`;
+
   return (
-    <div className="App">
-    <h1>Hello from App</h1>
+    <div className="app">
+    <div className="container">
+      <div className="top">
+        <div className="location">
+          <p>Antwerp</p>
+        </div>
+        <div className="temp">
+          <h1>14°C</h1>
+        </div>
+        <div className="description">
+          <p>Sunny</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p>16°C</p>
+        </div>
+        <div className="humidity">
+          <p>20%</p>
+        </div>
+        <div className="wind">
+          <p>14 km/h</p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
